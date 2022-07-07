56c42d0a5e3270bdc3e93e91e761def0c852a760
Nathan Ramli <hezkyanatanael84@gmail.com>
2022-07-07 14:43:10 +0700

    feat: add vaccine CRUD API

---
+++ src/main/java/com/evizy/evizy/domain/dto/VaccineRequest.java
@@ -0,0 +1,26 @@
+package com.evizy.evizy.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class VaccineRequest implements Serializable {
+    private static final long serialVersionUID = -2717849663333213980L;
+
+    private Long id;
+    private String name;
+}
